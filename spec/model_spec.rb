4b21b924bf2d6dec8533587480d7b72676aaa213
John Z Wu <bogardon@gmail.com>
2013-07-10 10:00:26 -0700

    [model_spec] test more stuff

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -5,6 +5,7 @@ describe "Model" do
         "id" => 1,
         "title" => "Projectile",
         "body" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "created_at" => "2013-08-10T07:00:00-07:00",
         "comments" => [
           {
             "comment" => {
@@ -43,7 +44,11 @@ describe "Model" do
 
   describe "populating relationships" do
     it "has correct relationships" do
-      @post.comments.count.should == 2
+      @comments.count.should == 2
+      @comments.first.id.should == 1
+      @comments.last.id.should == 2
+      @comments.first.text.should == "Cool story bro."
+      @comments.last.text.should == "La Li Lu Le Lo"
     end
   end
 
